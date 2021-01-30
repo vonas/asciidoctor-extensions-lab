b0a42dc7c09fe9412f2bc711a4d5dbb151df5ac7
vonas <24623262+vonas@users.noreply.github.com>
2021-01-30 17:01:19 +0100

    Add bug example

--- lib/tex-preprocessor/extension.rb
+++ lib/tex-preprocessor/extension.rb
@@ -4,16 +4,9 @@ include Asciidoctor
 
 class TeXPreprocessor < Extensions::Preprocessor
 
-  # Map $...$ to stem:[...]
-  TEX_DOLLAR_RX = /(^|\s|\()\$(.*?)\$($|\s|\)|,|\.)/
-  TEX_DOLLAR_SUB = '\1latexmath:[\2]\3'
-
   def process document, reader
     return reader if reader.eof?
-    replacement_lines = reader.read_lines.map do |line|
-      (line.include? '$') ? (line.gsub TEX_DOLLAR_RX, TEX_DOLLAR_SUB) : line
-    end
-    reader.unshift_lines replacement_lines
+    reader.unshift_lines reader.read_lines  # this is the culprit
     reader
   end
 
